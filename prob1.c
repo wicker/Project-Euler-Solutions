ffd5a1fd395d7a2b8fb1bdf2a382e400ca7266a4
Jen Hanni <jeh.wicker@gmail.com>
2011-08-22 14:23:48 -0700

    solved first problem

---
+++ prob1.c
@@ -0,0 +1,26 @@
+/* Author: Jen Hanni
+ * Problem 1
+ * Language: C
+ */
+
+#include <stdio.h>
+
+void main(void)
+{
+  int count = 1;
+  int sum = 0;
+
+  while (count < 16)
+  {
+    if (count % 3 == 0 || count % 5 == 0)
+    {
+        sum = sum + count;
+        count++;
+    }
+    else 
+    {
+        count++;
+    }
+  }
+  printf("%d\n",sum);
+}
