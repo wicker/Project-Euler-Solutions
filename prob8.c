ece3bfad6c1b24b3ffd21b8eff662869a4ca1dcd
Jenner Hanni <jeh.wicker@gmail.com>
2012-12-31 18:04:01 -0800

    Got hung up in the integer conversion of a char. Successfully returns the largest product of five consecutive digits in a given 1000-digit number.

--- prob8.c
+++ prob8.c
@@ -22,13 +22,40 @@ int main(int argc, char *argv[]) {
   }
 
   char str[SIZE];
+  int i,j,a,b,c,d,e;
+  int product = 1;
+  int winner = 1;
+  char test[5];
 
   FILE *fp = fopen(argv[1], "r");
   fgets(str, SIZE, fp); 
 
+  // increment the starting digit i
+  // copy the five digits starting at i to the variable test
+  // find product of the five digits in test
+  // compare product to existing largest product and update if needed
+  // finish checking entire string and print result
 
+  for (i = 0; i < SIZE - 5; i++)
+  {
+      strncpy(test,str+i,5);
+      test[5] = '\0';
 
-  printf("str: %s\n",str);
+      a = test[0] - '0';
+      b = test[1] - '0';
+      c = test[2] - '0';
+      d = test[3] - '0';
+      e = test[4] - '0';
+      product = a * b * c * d * e;
+
+      printf("a: %d b: %d test: %s product: %d winner: %d\n",a,b,test,product,winner);
+      
+      if (product > winner)
+          winner = product;
+      product = 1;
+  }
+
+  printf("winner: %d\n",winner);
   return 0;
 
 }
