ceac2b316b32bb4c4ba899de5fa55cb5d0b30845
Jenner Hanni <jeh.wicker@gmail.com>
2013-01-02 15:22:00 -0800

    Implemented the sieve to mark indices in the array as 1 or 0 but it isn't working past 2.

--- prob10-sieve.c
+++ prob10-sieve.c
@@ -10,9 +10,11 @@
 #include <math.h>
 #include <string.h>
 
-#define NUMTOFIND 2000001
+#define NUMTOFIND 11
 
 int check_if_prime(int);
+
+void mark_all_primes(int, int *);
 
 int main() {
 
@@ -22,40 +24,27 @@ int main() {
   // the list is initialized with all 0 and every composite number then marked 
 
   int p,i; 
-  char numbers[NUMTOFIND];
+  int numbers[NUMTOFIND];
+  int *ptr;
+  ptr = numbers;
 
-  int num;
+  int num, base;
 
   // set the entirety of numbers to zero
   for (p = 0; p < NUMTOFIND; p++)
       numbers[p] = 0;
 
-  // starting with p = 2, walk through all numbers and mark as prime or not prime
-  for (p = 2; p < NUMTOFIND; p++)
+  // starting with number two, walk through all numbers and mark as not prime
+  for (i = 2; i < NUMTOFIND; i++)
   {
-    for (i = p; i < NUMTOFIND; i = i + p)
-    {
-       if (numbers[i] == 0) // if we still think it might be prime
-       {
-          if (check_if_prime(i) == 1)
-              numbers[i] = 1;
-       }
-    }
-  }
-
-  int sum = 0;
-
-  for (i = 0; i < NUMTOFIND; i++)
-  {
-      num = numbers[i]; 
-      if (num == 0)
-         sum = sum + i;
-  }
-
+      mark_all_primes(i, ptr);
+      printf("all nums divisible by %d are now labeled 1.\n",i);
   for (p = 0; p < NUMTOFIND; p++)
-      printf("%d\n",numbers[p]);
+      printf("num: %d is %d\n",p,numbers[p]);
+  }
 
-  printf("sum: %d\n",sum);
+
+//  printf("sum: %d\n",sum);
 
   return 0;
 
@@ -64,16 +53,20 @@ int main() {
 // check_if_prime(int i) returns a 0 if an integer is not
 // divisible by any number besides 1 and itself.   
 
-int check_if_prime(int i) {
-  int j = 2;
+int check_if_prime(int numtocheck) {
+  int testdigit = 2;
   int k = 0;
   int not_prime = 0;
 
-  for (j = 2; j < i - 1; j++)
+  for (testdigit = 2; testdigit < NUMTOFIND; testdigit++)
   {
-    k = i % j;
+    k = numtocheck % testdigit;
+    printf("num: %d k: %d\n",numtocheck,testdigit);
     if (k == 0)
+       {
        not_prime++;
+       break;
+       }
   }
 
   if (not_prime == 0)
@@ -81,4 +74,45 @@ int check_if_prime(int i) {
   else
      return 1;
 }
+
+void mark_all_primes(int base, int *ptr) 
+{
+  int i;
+  for (i = i + base; i < NUMTOFIND; i = i + base)
+  {
+      ptr[i] = 1;
+  }
+}
+
+/*
+      base = i;
+      for (x = i; x < NUMTOFIND; x++)
+          numbers[x] = 1;
+
+ 
+
+       if (numbers[i] == 0) // if we still think it might be prime
+       {
+          if (check_if_prime(i) == 1)
+              numbers[i] = 1;
+       }
+  }
+
+
+  for (p = 3; p < NUMTOFIND; p++)
+  {
+    for (i = p; i < NUMTOFIND; i = i + p)
+    {
+    }
+  }
+
+  int sum = 0;
+
+  for (i = 2; i < NUMTOFIND; i++)
+  {
+      num = numbers[i]; 
+      if (num == 0)
+         sum = sum + i;
+  }
+*/
 
