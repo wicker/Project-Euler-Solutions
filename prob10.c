23879831e3f228097b087c172980f97060a9d3fb
Jenner Hanni <jeh.wicker@gmail.com>
2014-12-13 16:09:35 -0800

    Done! We were overflowing so used a double. Accepted.

--- prob10.c
+++ prob10.c
@@ -1,51 +1,65 @@
-/* Author: Jenner Hanni
- * Copyright 2012, MIT License. <jeh.wicker@gmail.com>
- * Problem 10
- * Find the sum of all primes below two million
- * Language: C
- */
+// Project Euler #10
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
-#include <string.h>
 
-int check_if_prime(int);
+#define ERROR -1
 
-int main() {
+#define PRIME 0
+#define NOTPRIME 1
 
-  int num;
-  int sum = 0;
+void print_all_primes(unsigned int max);
+short is_it_prime(unsigned int num);
 
-  for (num = 0; num < 2000001; num++)
-  {
-      if (check_if_prime(num) == 0)
-         sum = sum + num;
+// this works for unsigned ints
+// 0 <= num <= 2,147,483,647
+short is_it_prime(unsigned int num) {
+  unsigned int i;
+  unsigned long int actual = sqrt(num);
+
+  if (num < 2) return NOTPRIME;
+  else if (num == 2) return PRIME;
+
+  for (i = 2; i <= actual+1; i++) {
+    if (num % i == 0) {
+      return NOTPRIME;
+    }
+  }
+  return PRIME;
+}
+
+// print all the primes for a number
+void print_all_primes(unsigned int max) {
+
+  unsigned int i,inc;
+  double sum;
+  unsigned int actual = sqrt(max) - 1;
+  short *ptr = calloc (max, sizeof(short*));
+
+  for (i = 2; i <= max; i++) {
+    ptr[i] = is_it_prime(i);
+    inc = i;
+    while (i <= max) {
+      i += inc;
+      ptr[i] = NOTPRIME;
+    }
+    i = inc;
   }
 
-  printf("numbers checked: %d\nsum: %d\n",num,sum);
+  sum = 0;
+  for (i = 2; i <= max; i++) {
+    if (ptr[i] == PRIME) 
+      sum += i;
+  }
+  printf("sum: %.0f\n",sum);
+
+  free(ptr);
+}
+
+int main(int argc, char* argv[]) {
+
+  print_all_primes(atoi(argv[1]));
 
   return 0;
-
 }
-
-// check_if_prime(int i) returns a 0 if an integer is not
-// divisible by any number besides 1 and itself.   
-
-int check_if_prime(int i) {
-  int j = 2;
-  int k = 0;
-  int not_prime = 0;
-
-  for (j = 2; j < i - 1; j++)
-  {
-    k = i % j;
-    if (k == 0)
-       not_prime++;
-  }
-
-  if (not_prime == 0)
-     return 0;
-  else
-     return 1;
-}
-
